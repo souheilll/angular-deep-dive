d2eb7f4ae6a188bcef7cc08b801823035cd1c432
souheil <souheilmansouri06@gmail.com>
2026-07-20 21:25:10 +0100

    introdcing angular lifecycle hook ngonInit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'offline'|'online'|'unknown'  = 'offline';
 
   constructor(){
-    setInterval(()=>{
+  
+  }
+  
+  ngOnInit(): void {
+      setInterval(()=>{
      const rdm=Math.random()
      if(rdm<0.5){
        this.currentStatus='online'
